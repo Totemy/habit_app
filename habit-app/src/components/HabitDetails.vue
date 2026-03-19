a8c7d1d7761d6bff14e56d3605e41cc119156342
Totemy <thetotemofficial@gmail.com>
2026-03-19 17:54:46 +0200

    Added show  success

--- habit-app/src/components/HabitDetails.vue
+++ habit-app/src/components/HabitDetails.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { ref, Teleport, watch } from 'vue'
 import { useHabitItems } from '../composables/useHabitItems'
 import type { Habit } from '../types/Habit'
 import HabitItem from './HabitItem.vue'
@@ -7,8 +8,14 @@ const props = defineProps<{
   habit: Habit
 }>()
 
-const { handleClick, progressCount, progressPercent, done, undo } =
+const { handleClick, progressCount, progressPercent, done, undo, reset } =
   useHabitItems(() => props.habit.items)
+
+const showSuccess = ref(false)
+
+watch(progressPercent, (value) => {
+  if (value === 100) showSuccess.value = true
+})
 </script>
 <template>
   <div class="card p-6">
@@ -37,7 +44,19 @@ const { handleClick, progressCount, progressPercent, done, undo } =
     <div>
       <button class="button" @click="done">Done</button>
       <button class="button" @click="undo">Undo</button>
+      <button class="button" @click="reset">Reset</button>
     </div>
+    <Teleport to="body">
+      <div
+        v-if="showSuccess"
+        class="fixed inset-0 bg-black/50 flex items-center justify-center z-50"
+      >
+        <div class="card p-8 flex flex-col items-center gap-4">
+          <p class="text-2x1 font-bold">All done!!!</p>
+          <button class="button" @click="showSuccess = false">Ok</button>
+        </div>
+      </div>
+    </Teleport>
   </div>
 </template>
 
